0c5e1e61df2f9f5c40f07a28116a2bb58e6d8a15
Hồng Thất Công <67528122+HongThatCong@users.noreply.github.com>
2024-03-14 10:52:54 +0700

    Add new file ida_fuzzy.py

--- ida_fuzzy.py
+++ ida_fuzzy.py
@@ -1,13 +1,21 @@
-from idaapi import Form
-from idaapi import Choose
-import idaapi
-from ida_kernwin import *
-from fuzzywuzzy import process, fuzz
-from idautils import *
+# homepage: https://github.com/Ga-ryo/IDAFuzzy
+
+# pylint: disable=C0103,C0111,C0301
+
+from __future__ import print_function
+
 import gc
 import functools
-from PyQt5 import QtCore
 import threading
+from thefuzz import process, fuzz
+
+import idc
+import idaapi
+import idautils
+
+from idaapi import Form
+from idaapi import Choose
+from PyQt5 import QtCore
 
 """
 Fuzzy Search v1.0
@@ -41,36 +49,34 @@ Choose.CH_QFTYP_FUZZY is not so usable.
 
 LISTLEN = 10
 
-class Commands(object):
+
+class Commands:
     """
     Command execution proxy.
     """
 
     def __init__(self, **kwargs):
         self.kwargs = kwargs
-        assert (callable(kwargs['fptr']))
-        # assert(kwargs.get('description') != None)
+        assert callable(kwargs["fptr"])
 
     @property
     def description(self):
-        return self.kwargs.get('description')
+        return self.kwargs.get("description")
 
     def execute(self):
-        # ea = get_screen_ea()
-        # open_strings_window(ea)
-        if self.kwargs.get('args') is not None:
-            self.kwargs.get('fptr')(*self.kwargs.get('args'))
+        if self.kwargs.get("args") is not None:
+            self.kwargs.get("fptr")(*self.kwargs.get("args"))
         else:
-            self.kwargs.get('fptr')()
+            self.kwargs.get("fptr")()
 
     def get_icon(self):
-        if self.kwargs.get('icon') is None:
+        if self.kwargs.get("icon") is None:
             return 0
-        return self.kwargs.get('icon')
+        return self.kwargs.get("icon")
 
 
-choices = {}
-names = []
+choices: dict = {}
+names: list = []
 
 
 class EmbeddedChooserClass(Choose):
@@ -78,85 +84,66 @@ class EmbeddedChooserClass(Choose):
     A simple chooser to be used as an embedded chooser
     """
 
-    def __init__(self, title, nb=5, flags=0):
-        Choose.__init__(self,
-                        title,
-                        [["Action", 30 | Choose.CHCOL_PLAIN]],
-                        embedded=True, height=10, flags=flags)
-        # embedded=True, width=30, height=20, flags=flags)
-
+    def __init__(self, form, title, flags=0):
+        super().__init__(title, [["Action/Name", 30 | Choose.CHCOL_PLAIN]], embedded=True, height=20, flags=flags)
+        self.form = form
         self.n = 0
         self.items = []
         self.icon = 0
 
     def OnGetIcon(self, n):
-        # print("get icon %d" % n)
         return choices[self.items[n][0]].get_icon()
 
-    def OnSelectionChange(self, n):
-        pass
-        # print("selection change %d" % n)
-
-    def OnClose(self):
-        pass
+    def OnSelectLine(self, n):
+        if 0 <= n < len(self.items):
+            self.form.Close(1)
 
     def OnGetLine(self, n):
-        # print("getline %d" % n)
-        return self.items[n]
+        if 0 <= n < len(self.items):
+            return self.items[n]
+        return None
 
     def OnGetSize(self):
-        n = len(self.items)
-        # print("getsize -> %d" % n)
-        return n
+        return len(self.items)
 
-"""
-def hooked_scorer(*args,**kwargs):
-    # watch event every time.
-    pass
-
-def hook(function, prefunction):
-    @functools.wraps(function)
-    def run(*args, **kwargs):
-        prefunction(*args, **kwargs)
-        return function(*args, **kwargs)
-    return run
-fuzz.WRatio = hook(fuzz.WRatio, hooked_scorer)
-
-"""
 
 class TerminateException(Exception):
     pass
 
+
 def hooked_scorer(*args, **kwargs):
-    if kwargs.pop('terminate_event').is_set():
+    if kwargs.pop("terminate_event").is_set():
         raise TerminateException
     return fuzz.WRatio(*args, **kwargs)
 
+
 class FuzzySearchThread(QtCore.QThread):
-    refresh_list = QtCore.pyqtSignal([str]*LISTLEN)
+    refresh_list = QtCore.pyqtSignal([str] * LISTLEN)
 
     def __init__(self, parent=None):
-        super(FuzzySearchThread, self).__init__(parent)
+        super().__init__(parent)
         self.stopped = False
         self.mutex = QtCore.QMutex()
         self.terminate_event = threading.Event()
 
-    def setup(self,  s):
-        self.stoppped = False
+    def setup(self, s):
+        self.stopped = False
         self.s = s
 
-    def stop( self ):
-        with QtCore.QMutexLocker( self.mutex ):
+    def stop(self):
+        with QtCore.QMutexLocker(self.mutex):
             self.stopped = True
 
     def run(self):
         f = functools.partial(hooked_scorer, terminate_event=self.terminate_event)
         try:
-            res = process.extract(self.s, names, limit=LISTLEN, scorer=f)  # f.iStr1.value won't change until Form.Execute() returns.
+            res = process.extract(
+                self.s, names, limit=LISTLEN, scorer=f
+            )  # f.iStr1.value won't change until Form.Execute() returns.
             extracts = []
             for i in res:
                 extracts.append(i[0])
-            for i in range(10-len(res)):
+            for i in range(10 - len(res)):
                 extracts.append("")
             self.refresh_list.emit(*extracts)  # call main Thread's UI function.
         except TerminateException:
@@ -165,36 +152,31 @@ class FuzzySearchThread(QtCore.QThread):
         self.finished.emit()
 
 
-# --------------------------------------------------------------------------
 class FuzzySearchForm(Form):
     def __init__(self):
         self.invert = False
-        self.EChooser = EmbeddedChooserClass("Title", flags=Choose.CH_MODAL)
+        self.EChooser = EmbeddedChooserClass(self, "", flags=Choose.CH_MODAL)
         self.selected_id = 0
         self.s = ""
         self.fst = FuzzySearchThread()
         self.fst.refresh_list.connect(self.refresh_list)
         self.fst.finished.connect(self.finished)
-        # self.EChooser = EmbeddedChooserClass("Title", flags=Choose.CH_CAN_REFRESH)
 
-	# Portability fix from Python2 to Python3.
-        try:
-            self.cEChooser = super().cEChooser #super() will raise exception in python2
-        except:
-            pass
+        Form.__init__(
+            self,
+            r"""STARTITEM {id:iStr1}
+IDA Fuzzy Search
+{FormChangeCb}
+<:{iStr1}>
 
-        Form.__init__(self, r"""STARTITEM 
-        IDA Fuzzy Search
-        {FormChangeCb}
-        <:{iStr1}>
-
-        <Results:{cEChooser}>
-""", {
-            'iStr1': Form.StringInput(),
-            'cEChooser': Form.EmbeddedChooserControl(self.EChooser),
-            'FormChangeCb': Form.FormChangeCb(self.OnFormChange),
-        })
-        # self.modal = False
+<Results:{cEChooser}>
+""",
+            {
+                "iStr1": Form.StringInput(),
+                "cEChooser": Form.EmbeddedChooserControl(self.EChooser),
+                "FormChangeCb": Form.FormChangeCb(self.OnFormChange),
+            },
+        )
 
     def OnFormChange(self, fid):
         if fid == -1:
@@ -204,22 +186,24 @@ class FuzzySearchForm(Form):
             # terminate
             pass
         elif fid == self.cEChooser.id:
-            self.selected_id = self.GetControlValue(self.cEChooser)[0]
+            value = self.GetControlValue(self.cEChooser)
+            if value:
+                self.selected_id = value[0]
         elif fid == self.iStr1.id:
             self.s = self.GetControlValue(self.iStr1)
             self.EChooser.items = []
-            if self.s == '':
+            if self.s == "":
                 self.RefreshField(self.cEChooser)
                 return 1
             self.fst.stop()
-            self.fst.quit()  #  if you type speedy, FuzzySearch which executed before is not finished here.
+            self.fst.quit()  # if you type speedy, FuzzySearch which executed before is not finished here.
             self.fst.terminate_event.set()
             self.fst.wait()
-            #self.fst.terminate()  # but last time's FuzzySearch is meaningless, so terminate this. <- little dangerous?
+            # self.fst.terminate()   # but last time's FuzzySearch is meaningless, so terminate this. <- little dangerous?
 
-            #stop and quit take time.(and maybe non-blocking)
-            #So if you type speedy, some start() call will be ignored.
-            #re-create thread solve this.
+            # stop and quit take time.(and maybe non-blocking)
+            # So if you type speedy, some start() call will be ignored.
+            # re-create thread solve this.
             self.fst = FuzzySearchThread()
             self.fst.refresh_list.connect(self.refresh_list)
             self.fst.finished.connect(self.finished)
@@ -233,7 +217,6 @@ class FuzzySearchForm(Form):
 
     def refresh_list(self, *extracts):
         for ex in extracts:
-            # self.EChooser.items.append([ex[0], choices[ex[0]].description])
             self.EChooser.items.append([ex])
         self.RefreshField(self.cEChooser)
         self.SetControlValue(self.cEChooser, [0])  # set cursor top
@@ -242,18 +225,18 @@ class FuzzySearchForm(Form):
         pass
 
     def get_selected_item(self):
-        if self.selected_id == -1:
+        if (self.selected_id == -1) or (self.selected_id >= len(self.EChooser.items)):
             return None
         item_name = self.EChooser.items[self.selected_id][0]
         return choices[item_name]
 
 
-# --------------------------------------------------------------------------
 def fuzzy_search_main():
     # Create form
-    global f, choices, names
-    choices = {}
-    names = []
+
+    choices.clear()
+    names.clear()
+
     gc.collect()
 
     # Runtime collector.
@@ -265,30 +248,85 @@ def fuzzy_search_main():
     # 1. Become slow in case large file.
     # 1.1. Re-generate dictionary isn't matter.(But scoring time will be bigger.)
     # func ptr and icon id
-    registered_actions = get_registered_actions()
+
+    registered_actions = idaapi.get_registered_actions()
     for action in registered_actions:
         # IDA's bug? tilde exists many times in label. ex) Abort -> ~A~bort
         # So fix it.
-        label = get_action_label(action).replace('~', '')
-        icon = get_action_icon(action)[1]
-        desctription = get_action_tooltip(action)
-        if get_action_state(action)[1] > idaapi.AST_ENABLE:
+        label = idaapi.get_action_label(action).replace("~", "")
+        icon = idaapi.get_action_icon(action)[1]
+        description = idaapi.get_action_tooltip(action)
+        if idaapi.get_action_state(action)[1] > idaapi.AST_ENABLE:
             continue
-        choices[label] = Commands(fptr=process_ui_action, args=[action], description=desctription, icon=icon)
+        choices[label] = Commands(fptr=idaapi.process_ui_action, args=[action], description=description, icon=icon)
 
     # Functions()
     # Heads()
-    for n in Names():
-        demangled = idc.demangle_name(n[1], idc.get_inf_attr(idc.INF_SHORT_DN))
-        name = demangled if demangled else n[1]
-        # jump to addr
-        choices[name] = Commands(fptr=jumpto, args=[n[0]], description="Jump to " + name, icon=124)
+    for ea, name in idautils.Names():
+        if name:
+            demangled = idc.demangle_name(name, idc.get_inf_attr(idc.INF_SHORT_DN))
+            name = demangled if demangled else name
+            # jump to addr
+            choices[name] = Commands(fptr=idc.jumpto, args=[ea], description=f"Jump to {name}", icon=124)
 
-    for n in Structs():
-        choices[n[2]] = Commands(fptr=open_structs_window, args=[n[1]],
-                                 description="Jump to Structure definition of " + n[2], icon=52)
+    # Structs
+    for _, sid, struct_name in idautils.Structs():
+        choices[struct_name] = Commands(
+            fptr=idaapi.open_structs_window,
+            args=[sid, 0],
+            description=f"Jump to Structure definition of {struct_name}",
+            icon=52,
+        )
 
-    for k, v in choices.items():
+        # Struct members
+        sptr = idaapi.get_struc(sid)
+        ofs = idaapi.get_struc_first_offset(sptr)
+        while ofs != idaapi.BADADDR:
+            mid = idaapi.get_member_id(sptr, ofs)
+            struct_member_name = idaapi.get_member_fullname(mid)
+            if struct_member_name:
+                choices[struct_member_name] = Commands(
+                    fptr=idaapi.open_structs_window,
+                    args=[sid, ofs],
+                    description=f"Jump to Structure member definition of {struct_member_name}",
+                    icon=52,
+                )
+
+            ofs = idaapi.get_struc_next_offset(sptr, ofs)
+
+    # Enums
+    for idx in range(idaapi.get_enum_qty()):
+        eid = idaapi.getn_enum(idx)
+        enum_name = idaapi.get_enum_name(eid)
+        choices[enum_name] = Commands(
+            fptr=idaapi.open_enums_window,
+            args=[eid],
+            description=f"Jump to Enum definition of {enum_name}",
+            icon=1110,
+        )
+
+        # Enum members
+        class enum_member_cb(idaapi.enum_member_visitor_t):
+            def __init__(self, ename):
+                super().__init__()
+                self.enum_name = ename
+
+            def visit_enum_member(self, cid, value):
+                enum_member_name = idaapi.get_enum_member_name(cid)
+                enum_member_name = f"{self.enum_name}.{enum_member_name}"
+                choices[enum_member_name] = Commands(
+                    fptr=idaapi.open_enums_window,
+                    args=[cid],
+                    description=f"Jump to Enum member definition of {enum_member_name}",
+                    icon=1110,
+                )
+
+                return 0
+
+        enum_members = enum_member_cb(enum_name)
+        idaapi.for_all_enum_members(eid, enum_members)
+
+    for k, _v in choices.items():
         names.append(k)
 
     f = FuzzySearchForm()
@@ -296,24 +334,25 @@ def fuzzy_search_main():
     # Compile (in order to populate the controls)
     f.Compile()
     f.iStr1.value = ""
+
     # Execute the form
     ok = f.Execute()
 
     if ok == 1 and len(f.EChooser.items) > 0:
-        f.get_selected_item().execute()
+        cmd = f.get_selected_item()
+        if cmd:
+            cmd.execute()
+
     # Dispose the form
     f.Free()
 
 
 class fuzzy_search_handler(idaapi.action_handler_t):
     def __init__(self):
-        idaapi.action_handler_t.__init__(self)
+        super().__init__()
 
     def activate(self, ctx):
-        action = fuzzy_search_main()
-
-        if action:
-            idaapi.process_ui_action(action)
+        fuzzy_search_main()
         return 1
 
     def update(self, ctx):
@@ -321,22 +360,24 @@ class fuzzy_search_handler(idaapi.action_handler_t):
 
 
 class FuzzySearchPlugin(idaapi.plugin_t):
-    flags = idaapi.PLUGIN_FIX | idaapi.PLUGIN_HIDE
+    flags = idaapi.PLUGIN_HIDE
     comment = "Fuzzy search everything for IDA"
     help = "Fuzzy search everything"
-    wanted_name = "fuzzy search"
+    wanted_name = "Fuzzy Search"
     wanted_hotkey = ""
+    action_name = "fz:fuzzysearch"
 
     def init(self):
-        print("Fuzzy Search Plugin loaded.")
+        print(f"[{self.wanted_name}] plugin loaded")
         idaapi.register_action(
-            idaapi.action_desc_t("fz:fuzzysearch", "Fuzzy Search", fuzzy_search_handler(), "Shift+SPACE", "", -1))
+            idaapi.action_desc_t(self.action_name, self.wanted_name, fuzzy_search_handler(), "Shift-Space", "", -1)
+        )
 
         return idaapi.PLUGIN_KEEP
 
     def term(self):
-        idaapi.unregister_action("fz:fuzzysearch")
-        pass
+        idaapi.unregister_action(self.action_name)
+        print(f"[{self.wanted_name}] plugin terminated")
 
     def run(self, arg):
         pass
